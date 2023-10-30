624fa96f056bf84f65173b8fbd1e16d26fa2cd75
Julio Corradini <julcorradi@gmail.com>
2023-10-29 21:50:14 -0300

    Cambiando la función minimax() para que la IA funcione correctamente según el algoritmo minimax.

--- ta-te-ti.js
+++ ta-te-ti.js
@@ -1,3 +1,4 @@
+const { resolve } = require('path');
 const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
@@ -25,8 +26,54 @@ function jugadaGanadora (tablero, jugador) {
   }
 };
 
+function minimax(tablero, jugador) {
+  // Función auxiliar para evaluar el estado del juego y retornar su valor.
+  function evaluar(tablero) {
+    if (jugadaGanadora(tablero, jugadorAI)) {
+      return 1; // La IA gana
+    } else if (jugadaGanadora(tablero, jugadorHumano)) {
+      return -1; // El humano gana
+    } else if (!tablero.includes("-")) {
+      return 0; // Empate
+    }
+    return null; // El juego no ha terminado
+  }
+
+  // Evaluar el estado actual del juego.
+  const resultado = evaluar(tablero);
+
+  if (resultado !== null) {
+    return { valor: resultado, indice: -1 }; // Devolver el valor y un índice inválido.
+  }
+
+  // Inicializar una lista de resultados para almacenar los valores y los índices de las jugadas posibles.
+  const resultados = [];
+
+  // Generar las jugadas posibles y evaluarlas recursivamente.
+  for (let i = 0; i < tablero.length; i++) {
+    if (tablero[i] === "-") {
+      tablero[i] = jugador;
+      const valorDeRegreso = minimax(tablero, jugador === jugadorAI ? jugadorHumano : jugadorAI).valor;
+      resultados.push({ valor: valorDeRegreso, indice: i });
+      tablero[i] = "-";
+    }
+  }
+
+  // Determinar la mejor jugada basada en el jugador actual.
+  let mejorResultado;
+  if (jugador === jugadorAI) {
+    // La IA busca maximizar su valor.
+    mejorResultado = resultados.reduce((max, resultado) => (resultado.valor > max.valor ? resultado : max), { valor: -Infinity });
+  } else {
+    // El humano busca minimizar el valor de la IA.
+    mejorResultado = resultados.reduce((min, resultado) => (resultado.valor < min.valor ? resultado : min), { valor: Infinity });
+  }
+
+  return mejorResultado;
+}
+
 // Función miniMax.
-function minimax (tablero, jugador,) {
+/*function minimax (tablero, jugador) {
 
   var talbleroAuxiliar = tablero.slice();
   
@@ -53,10 +100,10 @@ function minimax (tablero, jugador,) {
 
       if(turnoJugadorAI) {
         turnoJugadorAI = !turnoJugadorAI;
-        valorDeRegreso = minimax (talbleroAuxiliar, jugadorAI).valor;
+         valorDeRegreso = minimax (talbleroAuxiliar, jugadorAI).valor;
       } else {
         turnoJugadorAI = !turnoJugadorAI;
-        valorDeRegreso = minimax (talbleroAuxiliar, jugadorHumano).valor;
+         valorDeRegreso = minimax (talbleroAuxiliar, jugadorHumano).valor;
       }
 
       if (valorDeRegreso > resultadoMiniMax.valor) {
@@ -73,7 +120,7 @@ function minimax (tablero, jugador,) {
   console.log(resultadoMiniMax);
   return resultadoMiniMax;
 
-};
+};*/
 
 // Función para marcar las fichas de la IA
 function jugarAI(tablero) {
@@ -86,27 +133,16 @@ function jugarAI(tablero) {
 
 
 // Turno del jugador humano
-function turnoHumano() {
-  
+async function turnoHumano() {
 
-    // Imprime el tablero por consola.
-    for (let i = 0; i < 3; i++) {
-      let row = "";
-      for (let j = 0; j < 3; j++) {
-        const index = i * 3 + j;
-        row += tablero[index] + " ";
-      }
-      console.log(row);
-    };
-  
+  return new Promise((resolve)=>{
+
     rl.question("Elija el casillero (del 1 al 9) que desea marcar: ", (input) => {
 
       const jugadaDeHumano = parseInt(input);
       if (tablero[jugadaDeHumano - 1] === "-") {
         tablero[jugadaDeHumano - 1] = "O";
       }
-
-      rl.close();
   
       // Imprime el tablero por consola.
       for (let i = 0; i < 3; i++) {
@@ -118,25 +154,19 @@ function turnoHumano() {
         console.log(row);
       };
 
+      resolve();
+
     });
-  
+
+  });
   
 };
 
 // Turno del jugador IA
 function turnoIA() {
   
+  return new Promise ((resolve)=> {
 
-    // Imprime el tablero por consola.
-    for (let i = 0; i < 3; i++) {
-      let row = "";
-      for (let j = 0; j < 3; j++) {
-        const index = i * 3 + j;
-        row += tablero[index] + " ";
-      }
-      console.log(row);
-    };
-  
     jugarAI(tablero);
   
     // Imprime el tablero por consola.
@@ -148,13 +178,38 @@ function turnoIA() {
       }
       console.log(row);
     };
-  
-    
+
+    resolve();
+
+  });
 };
 
-if(turnoJugadorAI) { // HAY QUE LOGRAR QUE LUEGO DEL TURNO DEL HUMANO VUELVA A LLAMARSE AL TURNO DE LA IA
+function juegoTerminado(tablero) {
+  return jugadaGanadora(tablero, jugadorAI) || jugadaGanadora(tablero, jugadorHumano) || !tablero.includes("-");
+};
 
-    turnoIA();
-    turnoHumano();
+async function jugarTaTeTi () {
+
+  while (!juegoTerminado(tablero)) {
+
+    if (turnoJugadorAI) {
+      console.log("turno de la IA");
+      await turnoIA();
+      turnoJugadorAI = false;
+
+    } else {
+      console.log("turno del Humano");
+      await turnoHumano();
+      turnoJugadorAI = true;
+
+    }
+
+  };
+
+  console.log("Juego terminado.");
+
+  rl.close();
 
 };
+
+jugarTaTeTi();
